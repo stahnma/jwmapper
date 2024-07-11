df844f17215dad1f64bb5f32be9d88762570db34
Michael Stahnke <stahnke@floxdev.com>
2024-07-10 19:56:54 -0500

    Update hubot regex
    This adds the ability to use his @mention name as well.

--- hubot-part.js
+++ hubot-part.js
@@ -2,13 +2,17 @@
 //   Responds with the output of an external command when it hears "where is james".
 //
 // Commands:
-//   hubot where is james - Runs the external command and responds with its output.
+//   where is james - Runs the external command and responds with its output.
 
 const { exec } = require('child_process');
 
+console.log("James module loaded");
+
 module.exports = (robot) => {
-  robot.hear(/^where\s?(the\s?(heck|fuck)\s?)?is\s?james\s?white/i, (res) => {
-    const command = "maps/1.py";
+    robot.hear(/^where\s?(the\s?(heck|fuck)\s?)?is\s?(@)?james(\.s\.)?\s?white/i, (res) => {
+
+    // TODO Maybe make this an env var
+    const command = "/var/lib/burrito/maps/1.py";
 
     exec(command, (error, stdout, stderr) => {
       if (error) {
@@ -19,3 +23,4 @@ module.exports = (robot) => {
     });
   });
 };
+
