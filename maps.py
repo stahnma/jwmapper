6fc057cbd5644eaa438b23556ce71b0259cf98d0
Michael Stahnke <stahnke@floxdev.com>
2024-07-10 19:58:15 -0500

    Update maps.py to only display james
    This limits the returned individuals sharing location with you to be only those
    matching James

--- maps.py
+++ maps.py
@@ -1,19 +1,18 @@
-import os
+
+import re
 from locationsharinglib import Service
 
-# Your google cookies.
-# I've used the cookies.txt local extension in Chrome to export my cookies
-cookies_file = './cookies.txt'
-
-# The email of the Google account you use to sign in to Google Maps
-#  not the person you're trying to locate
-google_email = os.environ['GOOGLE_EMAIL']
+# Replace 'path/to/cookies.txt' with the actual path to your cookies.txt file
+cookies_file = '/var/lib/burrito/maps/cookies.txt'
+google_email = 'mastahnke@gmail.com'
 
 # Create the service
 service = Service(cookies_file=cookies_file, authenticating_account=google_email)
 
 for person in service.get_all_people():
-    print(person.full_name  + " is approximately at " + person.address)
-    lat = person.latitude
-    lon = person.longitude
-    print("https://www.google.com/maps?q=" + str(lat) + "," + str(lon))
+    # Exclude anybody not matching james
+    if re.search(r'james', person.full_name, re.IGNORECASE):
+        print(person.full_name  + " is approximately at " + person.address)
+        lat = person.latitude
+        lon = person.longitude
+        print("https://www.google.com/maps?q=" + str(lat) + "," + str(lon))
